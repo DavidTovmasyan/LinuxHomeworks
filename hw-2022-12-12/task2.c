cb9f94e4816c070439e443d880992ff718d9020c
David Tovmasyan <david20031809@gmail.com>
2022-12-18 17:47:35 +0400

    v2

--- hw-2022-12-12/task2.c
+++ hw-2022-12-12/task2.c
@@ -7,28 +7,23 @@
 
 int s;
 int subArrSize;
-int maxDif = 0;
-int maxDay = 0;
-int curDay;
+int* dif;
+int* maxDay;
+int* maxDif;
+int thread_num;
 
-pthread_mutex_t mt;
-
-void * sumCounter(void *  arg){
-	pthread_mutex_lock(&mt);
+void* sumCounter(void* arg){
 	int* arr = (int*) arg;
-	for(int i = 0;i < subArrSize;++i){
-		s += arr[i];
-		if(abs(arr[i]) > maxDif){
-			maxDif = abs(arr[i]);
-			maxDay = curDay;
+	for(int i = 0;i<subArrSize; ++i){
+		dif[thread_num]+=arr[i];
+		if(abs(arr[i]) > maxDif[thread_num]){
+			maxDif[thread_num] = abs(arr[i]);
+			maxDay[thread_num] = thread_num * subArrSize + i;
 		}
-		++curDay;
 	}
-	pthread_mutex_unlock(&mt);
 }
 
 int main(int argc, char* argv[]){
-	pthread_mutex_init(&mt, NULL);
 	int t;
 	sscanf(argv[1], "%d", &s);
 	sscanf(argv[2], "%d", &t);
@@ -59,15 +54,30 @@ int main(int argc, char* argv[]){
 				subArr[j][k] = 0;
 		}
 	}
+	dif = (int*)calloc(t,sizeof(int));
+	maxDay = (int*)calloc(t,sizeof(int));
+	maxDif = (int*)calloc(t,sizeof(int));
 	for(i = 0;i<t;++i){
-		curDay = i*subArrSize;
+		thread_num = i;
 		pthread_create(&trInd[i], NULL, sumCounter, (void*) &subArr[i][0]);
 	}
 	for(i = 0;i<t;++i){
-		pthread_join(trInd[i], NULL);
+		pthread_join(trInd[i],NULL);
 	}
-	printf("%d %d\n", s, maxDay);
+	int finMaxDay = 0;
+	int finMaxDif = 0;
+	for(i = 0;i<t;++i){
+		if(maxDif[i] >= finMaxDif){
+			finMaxDif = maxDif[i];
+			finMaxDay = maxDay[i];
+		}
+		s += dif[i];
+	}
+	printf("%d %d\n", s, finMaxDay);
+
+	free(dif);
+	free(maxDay);
+	free(maxDif);
 	close(fd);
-	pthread_mutex_destroy(&mt);
 	return 0;
 }
