9bcf46701a6d81aad500c26f4eddc9247c1b4d6d
David Tovmasyan <david20031809@gmail.com>
2023-02-27 22:56:26 +0400

    v2

--- OSHomeworks/hw-2023-02-21/task1.cpp
+++ OSHomeworks/hw-2023-02-21/task1.cpp
@@ -7,14 +7,23 @@
 pthread_mutex_t mutex;
 std::string dest;
 
-void copy_string(std::string& s1, const std::string& s2){
-	s1 += s2;
+struct passed_values{
+	std::string sub_str;
+	int start_ind;
+	int end_ind;
+};
+
+//consider, that [start;end) includes in str and len(str2) = end-start
+void copy_string(std::string& str1, std::string str2,  int start, int end){
+	for(int i = start; i < end; ++i){
+		str1[i] = str2[i-start];
+	}
 }
 
 void* func(void* arg){
 	pthread_mutex_lock(&mutex);
-	std::string* temp = (std::string*) arg;
-	copy_string(dest, *temp);
+	passed_values* temp = (passed_values*) arg;
+	copy_string( dest, (*temp).sub_str, (*temp).start_ind, (*temp).end_ind);
 	pthread_mutex_unlock(&mutex);
 	return NULL;
 }
@@ -22,6 +31,7 @@ void* func(void* arg){
 int main(int argc, char* argv[]){
 	pthread_mutex_init(&mutex, NULL);
 	std::string src = argv[1];
+	dest.resize(src.size());
 	std::string strs[4];
 	for(std::size_t i = 0; i < src.size()/4;++i){
 		strs[0].push_back(src[i]);
@@ -36,8 +46,14 @@ int main(int argc, char* argv[]){
     	        strs[3].push_back(src[i]);
         }
 	pthread_t tr[4];
+	passed_values pv[4];
 	for(std::size_t i = 0;i < 4; ++i){
-		pthread_create(&tr[i], NULL, func, (void*)  &strs[i]);
+		pv[i].sub_str = strs[i];
+		pv[i].start_ind = ( i * src.size() ) / 4;
+		pv[i].end_ind = ( (i+1) * src.size() ) / 4;
+	}
+	for(std::size_t i = 0;i < 4; ++i){
+		pthread_create(&tr[i], NULL, func, (void*)  &pv[i]);
 	}
 	for(std::size_t i = 0;i < 4; ++i){
 		pthread_join(tr[i], NULL);
