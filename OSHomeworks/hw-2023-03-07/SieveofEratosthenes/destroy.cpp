1857428b9a6f8f2bb4ee018633977d392b47d5ec
David Tovmasyan <david20031809@gmail.com>
2023-03-07 00:13:48 +0400

    v1

---
+++ OSHomeworks/hw-2023-03-07/SieveofEratosthenes/destroy.cpp
@@ -0,0 +1,23 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <iostream>
+#include <unistd.h>
+#include <errno.h>
+#include <string>
+
+int main() {
+        std::string pathname = "shm_file_2";
+
+        // create a key for shared mem
+        key_t key = ftok(pathname.c_str(), 'a');
+        if(key < 0) {
+                std::cout << "ERROR: Cannot generate a key!!\n";
+                exit(errno);
+        }
+        int shmid = shmget(key, 300 * sizeof(bool), 0600);
+        shmctl(shmid, IPC_RMID, NULL);
+
+        return 0;
+}
+
